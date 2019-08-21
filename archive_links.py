3956c47eb30bdd09d3ad41d84e27614d8a9fe21d
Taylor <taylornarchibald@gmail.com>
2019-08-21 02:57:29 -0600

    Update 2019-08-21

--- archive_links.py
+++ archive_links.py
@@ -37,23 +37,52 @@ my_session = requests.Session()
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
 my_session.headers.update({'user-agent': user_agent})
 
-def grab_links(html_page):
-    links = set()
+def get_domain(url):
+    from urllib.parse import urlparse
+    # from urlparse import urlparse  # Python 2
+    parsed_uri = urlparse(url)
+    result = '{uri.netloc}'.format(uri=parsed_uri)
+    return result
+
+def grab_links(html_page, main_page, local_only=True, robust=False):
+    domain = get_domain(main_page)
+    links = set([main_page])
     soup = BeautifulSoup(html_page, features="lxml")
-    for link in soup.findAll('a'):
+    print(soup)
+    tag = None if robust else 'a'
+    for link in soup.findAll(tag):
         link = link.get('href')
-        if "proraftingtours.com" in link:
-            links.add(link)
-            logger.info(link)
+        if link:
+            if "."==link[0]:
+                link = domain + link[1:]
+            if domain in link or not local_only:
+                if filter_link(link):
+                    links.add(link)
+                    logger.info(link)
     return links
+
+def filter_link(link):
+    if link[0]=="#":
+        return False
+    elif "mailto" in link:
+        return False
+    else:
+        return True
 
 def archive_links(links):
     for link in links:
         page = my_session.get(f"https://web.archive.org/save/{link}")
         logger.info(page)
-        
+
 if __name__=='__main__':
-    main_page = "https://proraftingtours.com"
-    page = my_session.get(main_page)
-    links = grab_links(page.text)
-    archive_links(links)
+    #main_page = "https://proraftingtours.com"
+    website_list = ["https://proraftingtours.com"]
+    #website_list = "https://jimarchibald.com","https://jimarchibald.com/family/family.htm"
+    
+    for main_page in website_list:
+        local = False if "jim" in main_page else True # only archive links on domain
+        robust = True if "jim" in main_page else False # look for everything
+        page = my_session.get(main_page)
+        links = grab_links(page.text, main_page, local, robust)
+        print(links)
+        archive_links(links)
